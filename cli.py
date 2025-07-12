f959fa783e831a1376609f13e0b74e08a8d2bdfa
Malte-Christian Kuns <malte-christian.kuns@mailbox.tu-dresden.de>
2025-07-12 12:38:10 +0200

    change some descriptions and defaults of cli options and regroup the code a bit more

--- cli.py
+++ cli.py
@@ -20,35 +20,35 @@ def arguments(parser):
                         help="Whether to trust remote code for models/tokenizers that would require it")
 
     # Benchmark configuration
-    parser.add_argument("--frameworks", type=str, nargs='+', default=['hf_accelerate'],
+    parser.add_argument("--frameworks", type=str, nargs='+', default=['hf_accelerate', 'vllm', 'vllm_async', 'llmlingua', 'openllm', 'deepspeed'],
                         help='Inference frameworks to benchmark. Select form hf_accelerate, vllm, vllm_async, llmlingua, openllm, deepspeed')
     parser.add_argument("--tokenize-config", type=dict,
-                        default={"return_tensors": "pt", "padding": "max_length", "truncation": True, "return_token_type_ids": False})
-    parser.add_argument("--data", type=str, default='data/prompts_mini.txt',
+                        default={"return_tensors": "pt", "padding": "max_length", "truncation": True, "return_token_type_ids": None})
+    parser.add_argument("--data", type=str, default='data/prompts.txt',
                         help="Path to the prompts text file")
+    parser.add_argument("--num-samples", type=int, default=128,
+                    help="Amount of Prompts to sample from data")
+    parser.add_argument("--batch-size", type=int, default=1,
+                        help="Batch size for prompts")
     parser.add_argument("--input-len", type=int, default=128,
                         help="Number of input tokens per sample")
     parser.add_argument("--output-len", type=int, default=128,
                         help="Number of tokens to generate per sample")
     parser.add_argument("--dtype", type=str, default="float16",
-                        help="(unused?) Model data type. Select from available torch datatypes like float32, bfloat16")
+                        help="Model data type. Select from available torch datatypes like float32, bfloat16")
     parser.add_argument("--warm-up-reps", type=int, default=1,
                         help="Warm up repetitions per framework")
     parser.add_argument("--repeats", type=int, default=10,
                         help="Repetitions of inference benchmark per framework")
     parser.add_argument("--num-gpus", type=int, default=1,
                         help="Number of GPUs to use for benchmark")
-    parser.add_argument("--batch-size", type=int, default=1,
-                        help="Batch size for prompts")
     parser.add_argument("--generate-from-token", action="store_true",
                         help="BALI setting, measures inference speed from token ids with fixed input length")
-    parser.add_argument("--num-samples", type=int, default=128,
-                    help="Amount of Prompts to sample from data")
 
     # File I/O: config, results, loglevel
-    parser.add_argument("--output-dir", type=str, default="../results",
+    parser.add_argument("--output-dir", type=str, default="../results/example",
                         help="Results directory")
-    parser.add_argument("--config-file", type=str, default="default_config.json",
+    parser.add_argument("--config-file", type=str, default="example.json",
                         help="Config file for running the benchmark.")
     parser.add_argument("--save-slurm-config", action="store_true",
                         help="Save SLURM environment variables")
