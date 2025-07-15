9e72724cb8ee26ceeb1bfc1bd7fe55f95008c196
s6690609--tu-dresden.de <lena.jurkschat@tu-dresden.de>
2025-07-15 11:20:08 +0200

    random input token feature

--- acceleration_frameworks/acceleration_framework.py
+++ acceleration_frameworks/acceleration_framework.py
@@ -8,11 +8,16 @@ from timer import InferenceTimer
 
 
 class AccelerationFramework():
-    def __init__(self, config, data, generate_from_token: bool = True):
+    def __init__(self, config, data, generate_from_token: bool = True, random_tokens=True):
         self.timer = InferenceTimer()
         self.data = data
         self.config = config
         self.generate_from_token = generate_from_token
+        self.random_tokens = random_tokens
+        
+        if not self.generate_from_token:    
+            assert self.random_tokens == False, "Random tokens can only be used if generate_from_token is True."
+        
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         if self.device == "cpu":
             raise ValueError("No GPU was found. Exiting...")
@@ -29,7 +34,7 @@ class AccelerationFramework():
             self.tokenize_data()
             self.timer.stop_tokenize_timer()
 
-        if type(self).__name__ in ["OpenLLM", "VLLM_Async"]:
+        if type(self).__name__ in ["VLLM_Async"]:
             outputs = asyncio.run(self.generate())
         else:
             outputs = self.generate()
@@ -46,9 +51,11 @@ class AccelerationFramework():
                 'output_len'], f"Output length {outputs.shape[1]} of framework {self.__class__.__name__} does not match the configs output length!"
 
         return {'total_time': self.timer.total_prediction_time(),
+                'input_shape': self.tokenized_data[0].shape
                 'output_shape': outputs.shape,
                 'batch_size': self.config['batch_size'],
                 'generate_from_token': self.generate_from_token,
+                'random_tokens':self.random_tokens,
                 'setup_time': self.timer.time_for_setup(),
                 'tokenize_time': self.timer.time_for_pre_tokenization() if self.timer.tokenize_time else 'No pretokenization',
                 'token_transfer_time': self.timer.token_transfer_time() if self.timer.tokenize_time else 'No pretokenization',
@@ -56,7 +63,12 @@ class AccelerationFramework():
                 'time_per_token': self.timer.time_per_token(outputs),
                 'token_per_sec': self.timer.token_per_sec(outputs),
                 'num_output_token': self.timer.num_output_token,
-                'sequences/s': self.timer.seq_per_sec(outputs)}
+                'num_input_token': self.tokenized_data[0].nelement()*int(self.config['num_samples']/self.config['batch_size']) if self.timer.tokenize_time else 'No pretokenization', 
+                'sequences/s': self.timer.seq_per_sec(outputs),
+                'start_timestamp': self.timer.start_time,
+                'end_timestamp': self.timer.end_time,
+                'setup_timestamp': self.timer.setup_time,
+                'tokenize_timestamp': self.timer.tokenize_time}
 
     def tokenize_data(self):
         """
@@ -67,10 +79,20 @@ class AccelerationFramework():
             self.tokenizer = AutoTokenizer.from_pretrained(self.config['model_name'],
                                                            model_max_length=self.config['input_len'],
                                                            **self.config['tokenizer_init_config'])
-            self.tokenizer.pad_token = self.tokenizer.eos_token
-            for b in self.data:
-                inputs = self.tokenizer(b, **self.config['tokenize_config'])
-                tokenized_batch.append(inputs)
+
+            if self.random_tokens:
+                logging.info("Using Random tokens as input data")
+                vocab_size = self.tokenizer.vocab_size
+                
+                assert self.config['num_samples']%self.config['batch_size']==0, "The number of samples is not divisible by the batch size!"
+                batches = int(self.config['num_samples']/self.config['batch_size'])
+                for b in range(batches):
+                    tokenized_batch.append(torch.randint(0,vocab_size,(self.config['batch_size'],self.config['input_len'])))
+            else:
+                self.tokenizer.pad_token = self.tokenizer.eos_token
+                for b in self.data:
+                    inputs = self.tokenizer(b, **self.config['tokenize_config'])
+                    tokenized_batch.append(inputs)
         else:
             # take max seq len as input len per batch
             tokenizer = AutoTokenizer.from_pretrained(self.config['model_name'])
@@ -79,7 +101,6 @@ class AccelerationFramework():
                 inputs = tokenizer(b, padding='longest', return_tensors='pt')
                 tokenized_batch.append(inputs)
         self.timer.stop_pure_tokenization_timer()
-        logging.info(f"Input IDs shape per batch:{inputs['input_ids'].shape}")
 
         self.tokenized_data = [inputs.to(self.device) for inputs in tokenized_batch]
 
@@ -98,3 +119,4 @@ class AccelerationFramework():
 
     def postprocess_outputs(self, outputs):
         return outputs
+
