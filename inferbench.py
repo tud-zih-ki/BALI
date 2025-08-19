edbb316a83d9f2ba272307213d025c035d7ec580
s9037008 <s9037008@c2.capella.hpc.tu-dresden.de>
2025-08-19 22:57:27 +0200

    make plotting thesis-compliant

--- inferbench.py
+++ inferbench.py
@@ -187,6 +187,7 @@ class InferBench:
         prefill_times = {}
         decode_times = {}
 
+        plt.rcParams.update({'font.size': 14})
         for framework in token_timestamps:
             # Only HFAccelerate measures this as of now
             if token_timestamps[framework].size == 0:
@@ -207,21 +208,45 @@ class InferBench:
                 decode_times[framework] = [np.median(t) for t in token_timestamps[framework][1:]]
 
             xs = list(range(len(token_timestamps[framework])))
-            avgs = [np.average(t) for t in token_timestamps[framework]]
-            stdevs = [np.std(t) for t in token_timestamps[framework]]
-            plt.bar(xs, avgs, yerr=stdevs)
+            # avgs = [np.average(t) for t in token_timestamps[framework]]
+            # flops = [f / a for f, a in zip(self.flops.get_flops(), avgs)]
+            # stdevs = [np.std(t) for t in token_timestamps[framework]]
+            # plt.bar(xs, avgs, yerr=stdevs)
 
             # error bars could also represent min and max (which i think is more informative
             #   but doesn't align with other benchmark errors)
             #   (better yet, 10th and 90th percentiles to account for outliers, but i could not be bothered)
 
-            # mins = [avgs[i] - np.min(t) for i, t in enumerate(token_timestamps[framework])]
-            # maxs = [np.max(t) - avgs[i] for i, t in enumerate(token_timestamps[framework])]
-            # plt.bar(xs, avgs, yerr=(mins, maxs))
+            medians = [np.median(t) for t in token_timestamps[framework]]
+            tflops = [f * self.config["batch_size"] / (a * 1e12 * self.config["num_samples"]) for f, a in zip(self.flops.get_flops(), medians)]
+            mins = [medians[i] - np.percentile(t, 5) for i, t in enumerate(token_timestamps[framework])]
+            maxs = [np.percentile(t, 95) - medians[i] for i, t in enumerate(token_timestamps[framework])]
+            colors = ["indigo", "orange"]
+            patches = []
+            fig, ax1 = plt.subplots(figsize=(10, 4))
+            ax2 = ax1.twinx()
 
-            plt.ylabel("Token latency in $s$")
-            plt.xlabel("Output Token No.")
-            plt.title(f"Token generation latencies for {framework}")
+            if len(xs) > 100:
+                ax1.bar(xs, medians, yerr=(mins, maxs), color=colors[0], width=1.001)
+                ax2.scatter(xs, tflops, color=colors[1], s=3)
+            else:
+                ax1.bar(xs, medians, yerr=(mins, maxs), color=colors[0])
+                ax2.scatter(xs, tflops, color=colors[1])
+            patches.append(Patch(color=colors[0], label=f"Ausführungszeit"))
+            patches.append(Patch(color=colors[1], label=f"Rechenleistung"))
+            fig.legend(ncols=2, loc="outside upper center", handles=patches, frameon=False)
+
+            ax1.set_xlim(-0.5, max(0.5, max(xs) - 0.5))
+            ax1.set_ylim(0, None)
+            ax2.set_ylim(0, None)
+            ax1.set_ylabel("Tokenlatenz [s]", fontsize=14)
+            ax1.set_xlabel("Ausgabetoken Nr.", fontsize=14)
+            ax2.set_ylabel("Rechenleistung [TFLOPS]", fontsize=14)
+            # ax1.set_ylabel("Token batch latency in $s$", fontsize=14)
+            # ax1.set_xlabel("Output Token No.", fontsize=14)
+            # ax2.set_ylabel("TFLOPS", fontsize=14)
+            # plt.title(f"Batch latencies for {framework}")
+            plt.subplots_adjust(left=None, bottom=0.15, right=None, top=0.88)
             plt.savefig(os.path.join(self.config["output_dir"], f"token-timings-{framework}.png"), dpi=500)
             plt.savefig(os.path.join(self.config["output_dir"], "../latest/", f"token-timings-{framework}.png"), dpi=500)
             plt.close()
